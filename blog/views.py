bc473b0c19688c78d893c483682795d87b4d827e
박재석 <m2363@naver.com>
2021-10-18 12:17:57 +0900

    FBV로 블로그 포스트 상세페이지

--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,14 @@ def index(request):
             'contents' : posts,
         }
     )
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_page.html',
+        {
+            'post': post,
+        }
+    )
