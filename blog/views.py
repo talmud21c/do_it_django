d0a7ee2dba60a12137097c2fa6123cf1c9450153
박재석 <m2363@naver.com>
2021-10-18 12:55:57 +0900

    CBV로 블로그 포스트 목록페이지 수정

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'contents' : posts,
-        }
-    )
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'contents' : posts,
+#         }
+#     )
+
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
